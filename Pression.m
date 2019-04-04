6ee91c96cf72ac8f9ec6854c0433bc73c62003d5
MaxM2357 <33008868+MaxM2357@users.noreply.github.com>
2019-04-04 16:43:21 +0200

    Add files via upload

--- Pression.m
+++ Pression.m
@@ -1,14 +1,11 @@
-function [P,x,y] = Pression
+function [P] = Pression(u, v)
 
     rho = 1000;
-    [u,v] = vitesse;
-    
     taille = size(u);
+    
     for i=1: taille(1)
         for j=1:taille(2)
-            P((i-1)*taille(2)+j) = rho*sqrt(u(i,j)*u(i,j)+v(i,j)*v(i,j))/2;
-            x((i-1)*taille(2)+j) = i;
-            y((i-1)*taille(2)+j) = j;
+            P(i, j) = rho*sqrt(u(i,j)*u(i,j)+v(i,j)*v(i,j))/2;
         end
     end
 end
--- vitesse.m
+++ vitesse.m
@@ -1,16 +1,15 @@
-function [u,v,x,y] = vitesse
+function [u,v] = vitesse(F, dom, PAS)
 
-    [F,dom,h] = Laplacien;
     DIMENSIONS = size(dom);
     NB_LIGNES = DIMENSIONS(1);
     NB_COLONNES = DIMENSIONS(2);
+    u = zeros(NB_LIGNES -2, NB_COLONNES - 2);
+    v = zeros(NB_LIGNES -2, NB_COLONNES - 2);
     
     for i=2 : NB_LIGNES-1
         for j=2 : NB_COLONNES-1
-            v((i-1)*(NB_LIGNES-1)+j) = deriv(F(i-1,j), F(i,j), F(i+1,j), dom(i-1,j), dom(i,j), dom(i+1,j), h);
-            u((i-1)*(NB_LIGNES-1)+j) = deriv(F(i,j-1), F(i,j), F(i,j+1), dom(i,j-1), dom(i,j), dom(i,j+1), h);
-            x((i-1)*(NB_LIGNES-1)+j) = i;
-            y((i-1)*(NB_LIGNES-1)+j) = j;
+            v(i, j) = deriv(F(i-1,j), F(i,j), F(i+1,j), dom(i-1,j), dom(i,j), dom(i+1,j), PAS);
+            u(i, j) = deriv(F(i,j-1), F(i,j), F(i,j+1), dom(i,j-1), dom(i,j), dom(i,j+1), PAS);
         end
     end
     
