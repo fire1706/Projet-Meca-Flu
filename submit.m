67f83cc91c4459d5def652cc9f6f666e91fa13b6
fire1706 <46523392+fire1706@users.noreply.github.com>
2019-04-06 16:32:09 +0200

    Add files via upload

---
+++ submit.m
@@ -0,0 +1,31 @@
+function [psi,u,v] = submit(which)
+
+switch which
+    case 1
+        cl = dlmread('1-cl.txt', '\t');
+        dom = dlmread('1-dom.txt', '\t');
+        num = dlmread('1-num.txt', '\t');
+        PAS = 0.5;
+    case 2
+        cl = dlmread('2-cl.txt', '\t');
+        dom = dlmread('2-dom.txt', '\t');
+        num = dlmread('2-num.txt', '\t');
+        PAS = 0.5;
+    case 3
+        cl = dlmread('3-cl.txt', '\t');
+        dom = dlmread('3-dom.txt', '\t');
+        num = dlmread('3-num.txt', '\t');
+        PAS = 0.001;
+    case 4
+        cl = dlmread('4-cl.txt', '\t');
+        dom = dlmread('4-dom.txt', '\t');
+        num = dlmread('4-num.txt', '\t');
+        PAS = 0.01;
+end
+
+psi = Laplacien(num,dom,cl);
+
+[u,v] = vitesse(psi, dom, PAS);
+
+
+end
