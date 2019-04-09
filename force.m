7d1a10ae32327161eb36cae031a058dd2509ae2b
fire1706 <46523392+fire1706@users.noreply.github.com>
2019-04-09 23:58:43 +0200

    Update force.m

--- force.m
+++ force.m
@@ -14,10 +14,10 @@ fy = 0;
 
 for i=2:length(p)
     if x(i-1) ~= x(i) %si la normale a la surface est dirigee selon y
-        fy = fy - (p(i) + p(i-1)) / ( (x(i) - x(i-1)) * h );
+        fy = fy + (p(i) + p(i-1)) * ( (x(i) - x(i-1)) * h );
     end
     if y(i-1) ~= y(i) %si la normale a la surface est dirigee selon x
-        fx = fx + (p(i) + p(i-1)) / ( (y(i) - y(i-1)) * h );
+        fx = fx - (p(i) + p(i-1)) * ( (y(i) - y(i-1)) * h );
     end
 end
 end
